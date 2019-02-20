1248b96ce59682bbfe3674fb91ad226bc6196631
qiuggv <274429987@qq.com>
2019-02-20 21:09:21 +0800

    test

--- telecom/startup_openstack_exporter.sh
+++ telecom/startup_openstack_exporter.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#sudo docker run -p 8883:9103 --env-file my_file -it rakeshpatnaik/prometheus-openstack-exporter:v0.2 sh -c 'echo "10.142.90.83 controller">>/etc/hosts && /usr/local/bin/exporter/main.py'
-sudo docker run -p 8883:9103 --name "openstack-exporter" --env-file env_file -d telecom/prometheus-openstack-exporter:v0.3 sh -c 'echo "135.151.128.149 controller">>/etc/hosts && /usr/local/bin/exporter/main.py'
+sudo docker run -p 8883:9103 --name "openstack-exporter" --env-file env_file -d telecom/prometheus-openstack-exporter:v0.3 sh -c 'echo "xxx.xxx.xxx.xxx controller">>/etc/hosts && /usr/local/bin/exporter/main.py'
